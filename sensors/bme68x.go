69c9c49888f4b65e5163e183c08d1d58661c387e
Alex <12470950+alex-nt@users.noreply.github.com>
2023-02-05 10:07:59 +0200

    Typo

--- sensors/bme68x.go
+++ sensors/bme68x.go
@@ -650,7 +650,7 @@ func (bme68x *BME68X) GetSensorData() {
 		log.InfoLog.Printf("%v - metrics", bme68x.Data)
 		log.InfoLog.Printf("%v %v %v %v %v- metrics", bme68x.temperature, bme68x.humidity, bme68x.pressure, bme68x.gasResistance, bme68x.burnInGasData)
 	}
-}q
+}
 
 func sumFLOAT32(array []float32) float32 {
 	var result float32
